024c2aded963a782787d53f12ccc0f1af3e786cf
VidyaVinodK <vidyak4ever@gmail.com>
2022-08-01 16:10:56 +0530

    Updated page

--- src/components/Second_page/Details.jsx
+++ src/components/Second_page/Details.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './Details.css'
 
 function Details() {
+ 
   return (
     <div>
         <div className='searchpage'>
         <h4>Search the employee details</h4>
          
          <input type="search" className="search-bar" placeholder='name' />
-        
+         <button className="searchbutton" type="search" >search</button>
         </div>
 
     </div>
